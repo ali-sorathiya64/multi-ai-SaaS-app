7021fd480558900ef485cd7942c88a36b3c4897e
Ali sorathiya <alisorathiya970@gmail.com>
2026-08-30 11:15:18 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import connectCloudinary from './config/cloudinary.js';
 import userRoutes from './routes/user.routes.js';
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 
 await connectCloudinary();
